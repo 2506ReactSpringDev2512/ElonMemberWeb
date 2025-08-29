1431350b0743cbb97e83cb58b4b5f9e0b366ff6b
user1 <user1@DESKTOP-5R85Q1T>
2025-08-29 10:27:24 +0900

    [ADD] 회원 가입, 회원 정보 삭제 추가
    1. 회원 가입 기능 추가 INSERT
    2. 회원 정보 삭제 기능 추가 DELETE

--- src/main/java/com/elon/member/controller/SignUpServlet.java
+++ src/main/java/com/elon/member/controller/SignUpServlet.java
@@ -1,11 +1,15 @@
 package com.elon.member.controller;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
+import com.elon.member.model.service.MemberService;
+import com.elon.member.model.vo.Member;
 
 /**
  * Servlet implementation class SignUpServlet
@@ -25,16 +29,34 @@ public class SignUpServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("/WEB-INF/views/member/signup.jsp")
+		.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String memberId = request.getParameter("memberId");
+		String memberPwd = request.getParameter("memberPwd");
+		String memberName = request.getParameter("memberName");
+		String gender = request.getParameter("gender");
+		int age = Integer.parseInt(request.getParameter("age"));
+		String email = request.getParameter("email");
+		String phone = request.getParameter("phone");
+		String address = request.getParameter("address");
+		String hobby= request.getParameter("hobby");
+		Member member = new Member(memberId,  memberPwd,  memberName,  gender,  age,  email, phone,  address,  hobby);
+		MemberService mService = new MemberService();
+		int result = mService.insertMember(member);
+		if(result > 0) {
+			response.sendRedirect("/");
+		}else {
+			request.setAttribute("errorMsg", "회원 정보 입력이 완료되지 않았습니다.");
+			request.getRequestDispatcher("/WEB-INF/views/common/error.jsp")
+			.forward(request, response);
+		}
+		
 	}
 
 }
--- src/main/webapp/WEB-INF/views/member/delete.jsp
+++ src/main/webapp/WEB-INF/views/member/delete.jsp
@@ -4,9 +4,13 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>회원 정보 삭제</title>
 </head>
 <body>
-
+	<h1>회원 정보 삭제</h1>
+	<form action="/member/delete" method="post">
+		삭제할 회원 아이디 입력 : <input type="text" name="memberId">
+		<input type="submit" value="삭제">
+	</form>
 </body>
 </html>
