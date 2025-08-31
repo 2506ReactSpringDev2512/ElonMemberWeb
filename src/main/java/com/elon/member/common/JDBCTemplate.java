dc8b02e25806314bec36fd60f0127b5cedee7df7
user1 <user1@DESKTOP-GJMTAAP>
2025-09-01 08:48:54 +0900

    [ADD] 회원가입 기능 완료 - error.jsp : errorMsg 함수로 이동할 에러페이지를 추가함. - index.jsp : "1. 회원가입" a태그의 /member/signup 경로를 추가함. - signup.jsp : 폼의 action은 /member/signup이며, method는 post로 회원가입 형식에 맞게 name들을 입력해주어 DB에 추가할 수 있게 추가함. - JDBCTemplate.java : 오라클 DB의 JHJDBC계정의 주소, 유저, 비밀번호와 싱글톤 패턴의 추가, Connection 기능을 여러 쓰기 위한 메소드를 추가함. - Member.java : memberId, memberPwd, memberName, gender, age, email, phone, address, hobby, enrollDate의 선언과 생성자들을 추가함. - MemberDAO : 오라클 DB의 DB들의 값과 쿼리문을 사용하고 반환하기 위해 추가함. - MemberService.java : JDBCTemplate.java의 Connection 기능을 받아와 getConnection을 사용하였고, MemberDAO의 값을 가져와 다시 반환하기 위해 추가함. - SignUpServelet.java : signup.jsp의 경로를 통해 페이지를 불러와주고, 그 안의 폼 메소드가 post이므로 아래 doPost를 실행하여 SignUpServlet, MemberService, MemberDAO순으로 입력 데이터 전송과 다시 MemberDAO, MemberService, SignUpServlet순으로 전송 및 추가된 데이터 값들의 반환을 위해 추가함.

--- src/main/java/com/elon/member/common/JDBCTemplate.java
+++ src/main/java/com/elon/member/common/JDBCTemplate.java
@@ -1,5 +1,37 @@
 package com.elon.member.common;
 
-public class JDBCTemplate {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class JDBCTemplate {
+	private final String DRIVER_NAME = "oracle.jdbc.driver.OracleDriver";
+	private final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
+	private final String USER = "JHJDBC";
+	private final String PASSWORD = "JHJDBC";
+	
+	// 싱글톤 패턴
+	// 클래스의 인스턴스를 하나만 생성해서 공유
+	private static JDBCTemplate instance;
+	private JDBCTemplate() {}
+	
+	public static JDBCTemplate getInstance() {
+		if(instance == null) {
+			instance = new JDBCTemplate();
+		}
+		return instance;
+	}
+	
+	public Connection getConnection() {
+		Connection conn = null;
+		try {
+			Class.forName(DRIVER_NAME);
+			conn = DriverManager.getConnection(URL, USER, PASSWORD);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
 }
--- src/main/java/com/elon/member/model/service/MemberService.java
+++ src/main/java/com/elon/member/model/service/MemberService.java
@@ -1,5 +1,31 @@
 package com.elon.member.model.service;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.elon.member.common.JDBCTemplate;
+import com.elon.member.model.dao.MemberDAO;
+import com.elon.member.model.vo.Member;
+
 public class MemberService {
+
+	private JDBCTemplate jdbcTemplate;
+	private MemberDAO mDao;
+	
+	public MemberService() {
+		jdbcTemplate = JDBCTemplate.getInstance();
+		mDao = new MemberDAO();
+	}
+	
+	public int insertMember(Member member) {
+		int result = 0;
+		Connection conn = jdbcTemplate.getConnection();
+		try {
+			result = mDao.insertMember(member, conn);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
 
 }
